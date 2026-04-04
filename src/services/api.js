7ea666baf8ca307daec84b5251805e53948f133f
Nahabwe Brian <nahabwebryn@hotmail.com>
2026-04-05 00:30:15 +0300

    Changed the BASE URL to point to live as default

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import useAuthStore from "@/store/authStore"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://rental-api.askmoozo.com/api/v1",
     headers: { "Content-Type": "application/json" },
 })
 
@@ -34,7 +34,7 @@ api.interceptors.response.use(
 
                 // Try to get a new access token
                 const response = await axios.post(
-                    `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8081/api/v1"}/auth/refresh`,
+                    `${import.meta.env.VITE_API_BASE_URL || "https://rental-api.askmoozo.com/api/v1"}/auth/refresh`,
                     { refreshToken }
                 )
 
